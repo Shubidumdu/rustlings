c91308a9d06f481f8ba86489f9235e0c5c253172
Shubidumdu <skaty4870@naver.com>
2021-06-01 20:17:00 +0900

    Solve move_semantics5

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -2,13 +2,13 @@
 // Make me compile without adding any newlines or removing any of the lines.
 // Execute `rustlings hint move_semantics5` for hints :)
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
   let mut x = 100;
   let y = &mut x;
-  let z = &mut y;
-  y += 100;
-  z += 1000;
+  *y += 100;
+  let z = y;
+  *z += 1000;
   assert_eq!(x, 1200);
 }
