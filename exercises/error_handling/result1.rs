e6fd0a006d2824ab8161114841504032c5cc3036
Shubidumdu <skaty4870@naver.com>
2021-06-02 14:13:53 +0900

    Solve error_handling

--- exercises/error_handling/result1.rs
+++ exercises/error_handling/result1.rs
@@ -8,22 +8,26 @@ struct PositiveNonzeroInteger(u64);
 
 #[derive(PartialEq, Debug)]
 enum CreationError {
-    Negative,
-    Zero,
+  Negative,
+  Zero,
 }
 
 impl PositiveNonzeroInteger {
-    fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        Ok(PositiveNonzeroInteger(value as u64))
+  fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
+    match value {
+      value if value < 0 => Err(CreationError::Negative),
+      value if value == 0 => Err(CreationError::Zero),
+      _ => Ok(PositiveNonzeroInteger(value as u64)),
     }
+  }
 }
 
 #[test]
 fn test_creation() {
-    assert!(PositiveNonzeroInteger::new(10).is_ok());
-    assert_eq!(
-        Err(CreationError::Negative),
-        PositiveNonzeroInteger::new(-10)
-    );
-    assert_eq!(Err(CreationError::Zero), PositiveNonzeroInteger::new(0));
+  assert!(PositiveNonzeroInteger::new(10).is_ok());
+  assert_eq!(
+    Err(CreationError::Negative),
+    PositiveNonzeroInteger::new(-10)
+  );
+  assert_eq!(Err(CreationError::Zero), PositiveNonzeroInteger::new(0));
 }
