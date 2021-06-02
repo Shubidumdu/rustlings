55fde204873030d127c4d0910b67c993dbb528f7
Shubidumdu <skaty4870@naver.com>
2021-06-02 20:36:48 +0900

    Solve quiz3

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -10,21 +10,21 @@
 // I AM NOT DONE
 
 pub fn times_two(num: i32) -> i32 {
-    num * 2
+  num * 2
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn returns_twice_of_positive_numbers() {
-        assert_eq!(times_two(4), ???);
-    }
+  #[test]
+  fn returns_twice_of_positive_numbers() {
+    assert_eq!(times_two(4), 8)
+  }
 
-    #[test]
-    fn returns_twice_of_negative_numbers() {
-        // TODO replace unimplemented!() with an assert for `times_two(-4)`
-        unimplemented!()
-    }
+  #[test]
+  fn returns_twice_of_negative_numbers() {
+    // TODO replace unimplemented!() with an assert for `times_two(-4)`
+    assert_eq!(times_two(-2), -4)
+  }
 }
