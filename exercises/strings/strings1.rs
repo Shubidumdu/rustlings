f929141e29190f1f770ecdffd06d6382b45de922
Shubidumdu <skaty4870@naver.com>
2021-06-02 12:27:39 +0900

    Solve strings

--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -2,13 +2,17 @@
 // Make me compile without changing the function signature!
 // Execute `rustlings hint strings1` for hints ;)
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
-    let answer = current_favorite_color();
-    println!("My current favorite color is {}", answer);
+  let answer = current_favorite_color();
+  println!("My current favorite color is {}", answer);
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+  // 1. `to_string()` 메서드를 사용하는 방법
+  // "blue".to_string()
+
+  // 2. `String::from` 트레이트를 사용하는 방법
+  String::from("blue")
 }
