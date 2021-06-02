74fc91e98caab201a7c5755def034040bb2934a6
Shubidumdu <skaty4870@naver.com>
2021-06-02 19:52:23 +0900

    Solve options

--- exercises/option/option3.rs
+++ exercises/option/option3.rs
@@ -1,19 +1,20 @@
 // option3.rs
 // Make me compile! Execute `rustlings hint option3` for hints
 
-// I AM NOT DONE
+// I AM DONE
 
 struct Point {
-    x: i32,
-    y: i32,
+  x: i32,
+  y: i32,
 }
 
 fn main() {
-    let y: Option<Point> = Some(Point { x: 100, y: 200 });
+  let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
-    match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
-        _ => println!("no match"),
-    }
-    y; // Fix without deleting this line.
+  match y {
+    Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+    _ => println!("no match"),
+  }
+
+  y;
 }
