0bffff74915d994ea6205b777437227ec6424baa
Shubidumdu <skaty4870@naver.com>
2021-06-03 14:43:37 +0900

    Solve iterators

--- exercises/standard_library_types/iterators3.rs
+++ exercises/standard_library_types/iterators3.rs
@@ -6,7 +6,7 @@
 //    list_of_results functions.
 // Execute `rustlings hint iterators3` to get some hints!
 
-// I AM NOT DONE
+// I AM DONE
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum DivisionError {
@@ -24,8 +24,8 @@ pub struct NotDivisibleError {
 // Otherwise, return a suitable error.
 pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
   match (a, b) {
-    (a, b) if (a % b == 2) => Ok(a / b),
     (a, b) if (b == 0) => Err(DivisionError::DivideByZero),
+    (a, b) if (a % b == 0) => Ok(a / b),
     _ => Err(DivisionError::NotDivisible(NotDivisibleError {
       dividend: a,
       divisor: b,
@@ -37,18 +37,19 @@ pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
 // Desired output: Ok([1, 11, 1426, 3])
 fn result_with_list() -> Result<Vec<i32>, DivisionError> {
   let numbers = vec![27, 297, 38502, 81];
-  let division_results = numbers
+  let division_results: Vec<i32> = numbers
     .into_iter()
-    .map(|n| divide(n, 27))
-    .collect::<Vec<Result<i32, DivisionError>>>()?;
+    .map(|n| divide(n, 27).unwrap())
+    .collect();
   Ok(division_results)
 }
 
 // Complete the function and return a value of the correct type so the test passes.
 // Desired output: [Ok(1), Ok(11), Ok(1426), Ok(3)]
-fn list_of_results() -> () {
+fn list_of_results() -> Vec<Result<i32, DivisionError>> {
   let numbers = vec![27, 297, 38502, 81];
-  let division_results = numbers.into_iter().map(|n| divide(n, 27));
+  let division_results = numbers.into_iter().map(|n| divide(n, 27)).collect();
+  division_results
 }
 
 #[cfg(test)]
--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -3,32 +3,42 @@
 // I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
-    // Complete this function to return the factorial of num
-    // Do not use:
-    // - return
-    // Try not to use:
-    // - imperative style loops (for, while)
-    // - additional variables
-    // For an extra challenge, don't use:
-    // - recursion
-    // Execute `rustlings hint iterators4` for hints.
+  // Complete this function to return the factorial of num
+  // Do not use:
+  // - return
+  // Try not to use:
+  // - imperative style loops (for, while)
+  // - additional variables
+  // For an extra challenge, don't use:
+  // - recursion
+  // Execute `rustlings hint iterators4` for hints.
+  match num {
+    0 => 1,
+    1 => 1,
+    // by fold
+    // _ => (1..=num).fold(1, |a, b| a * b),
+    // by reduce
+    // _ => (1..=num).reduce(|a, b| a * b).unwrap(),
+    // by product
+    _ => (1..=num).product(),
+  }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn factorial_of_1() {
-        assert_eq!(1, factorial(1));
-    }
-    #[test]
-    fn factorial_of_2() {
-        assert_eq!(2, factorial(2));
-    }
+  #[test]
+  fn factorial_of_1() {
+    assert_eq!(1, factorial(1));
+  }
+  #[test]
+  fn factorial_of_2() {
+    assert_eq!(2, factorial(2));
+  }
 
-    #[test]
-    fn factorial_of_4() {
-        assert_eq!(24, factorial(4));
-    }
+  #[test]
+  fn factorial_of_4() {
+    assert_eq!(24, factorial(4));
+  }
 }
